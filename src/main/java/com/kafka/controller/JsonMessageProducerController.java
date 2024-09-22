e24ce28ae2e094501cc32530224484afcc1897c5
Aqeel Haider <aqeelhaider57@gmail.com>
2024-09-22 07:37:09 +0500

    springboot-kafka

---
+++ src/main/java/com/kafka/controller/JsonMessageProducerController.java
@@ -0,0 +1,26 @@
+package com.kafka.controller;
+
+import com.kafka.dto.User;
+import com.kafka.consumerandproducer.JsonKafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageProducerController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageProducerController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+}
